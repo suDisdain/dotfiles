29d0531f39ab15cb5f222c8c911446dd7078974b
Malachi Capela <malachicapela@gmail.com>
2020-07-02 07:10:41 +1000

    doom config + more

---
+++ .config/doom/config.el
@@ -0,0 +1,88 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Malachi Capela"
+      user-mail-address "malachicapela@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/Documents/org/")
+
+
+;; Sets the language for spell-checking
+(setq ispell-dictionary "en_AU")
+(setq ispell-program-name "aspell")
+(setq flyspell-default-dictionary ispell-dictionary)
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+
+;; Set the LSP servers
+(setq lsp-rust-server 'rust-analyzer)
+(setq rustic-lsp-server 'rust-analyzer)
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+
+
+(map! "<f12>" #'projectile-compile-project)
+
+
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("3577ee091e1d318c49889574a31175970472f6f182a9789f1a3e9e4513641d86" default))
+ '(safe-local-variable-values
+   '((eval setq-local flycheck-clang-include-path
+           (list
+            (expand-file-name "include"
+                              (projectile-project-root))
+            (expand-file-name "source/mon"
+                              (projectile-project-root))))
+     (eval setq flycheck-clang-include-path
+           (list
+            (expand-file-name "./include/"))))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- .zshenv
+++ .zshenv
@@ -1,7 +1,11 @@
 ZDOTDIR=$HOME/.config/zsh
 EDITOR="emacsclient -c -n -a=' '"
+BROWSER="firefox"
 export XAUTHORITY="$XDG_RUNTIME_DIR"/Xauthority
 export XDG_DATA_HOME="$HOME"/.local/share
+export XDG_CONFIG_HOME="$HOME"/.config
 export CARGO_HOME="$XDG_DATA_HOME"/cargo
 export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
-export PATH="$PATH:$HOME/.local/bin:$HOME/.local/share/cargo/bin"
+export GHCUP_INSTALL_BASE_PREFIX="$HOME"/.local/share
+export CLION_JDK=/opt/intellij-idea-ce/jbr/
+export PATH="$PATH:$HOME/.local/bin:$HOME/.local/share/cargo/bin:$HOME/.local/share/.ghcup/bin:$HOME/.cabal/bin:$HOME/.config/emacs/bin"
